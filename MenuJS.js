0a1e2131ed703f8bd1ddc5299467805b3f1a1b8a
Nicolas Pasino <niko_pasino@hotmail.com>
2024-04-03 14:28:32 -0300

    a

--- MenuJS.js
+++ MenuJS.js
@@ -4,11 +4,10 @@ class Menu extends HTMLElement {
     this.innerHTML = `
     <div>
       <nav><ul>
-        <li><a href="../index.html"> <img src="../Imgs/n.png" alt="n" height="20px" style="transform: scale(1.5);"></a></li>
+        <li><a href="../index.html"> <img src="/Imgs/logo.png" height="30px";"></a></li>
         <li><a href="https://nikitox24.github.io/Main/"> INICIO(GitHub) </a></li>
         <li><a href="https://github.com/NikitoX24/Main"> GITHUB </a></li>
         <li><a href="https://github.com/NikitoX24?tab=repositories"> REPOSITORIOS </a></li>
-        <li> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </li>
         <li><a href=#> (arriba) </a></li>
       </ul></nav>
     </div>
@@ -22,18 +21,21 @@ class Menu extends HTMLElement {
       }
       nav ul {
         background: linear-gradient(90deg, 
-        rgba(255, 255, 255, 0.5) 0%, 
-        rgba(255, 255, 255, 0.8) 25%, 
-        rgba(255, 255, 255, 0.8) 75%, 
-        rgba(255, 255, 255, 0.5) 100%);
+          rgba(255, 255, 255, 0.5) 0%, 
+          rgba(255, 255, 255, 0.8) 25%, 
+          rgba(255, 255, 255, 0.8) 75%, 
+          rgba(255, 255, 255, 0.5) 100%);
         box-shadow: 0 0 25px rgba(0, 0, 0, 0.1),
         inset 0 0 1px rgba(255, 255, 255, 0.6);
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
       }
       nav ul li {
-        display: inline-block;  
+        display: inline-block;
       }
       nav ul li a {
-        padding: 10px; 
+        padding: 10px;
         color: #000000;
         font-size: 18px;
         font-family: Arial;
@@ -42,7 +44,7 @@ class Menu extends HTMLElement {
       }
       li a:hover{
         color: red;
-        transform: scale(1.1)
+        transform: scale(1.1);
       }
     </style>
     `
