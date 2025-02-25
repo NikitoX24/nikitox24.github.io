0c35f9f5779a9d1337b17d5ba29562fd1ef9f83d
Nicolas Pasino <niko_pasino@hotmail.com>
2025-02-25 12:43:29 -0300

    cheats icons fix

--- myconfig/cheats.js
+++ myconfig/cheats.js
@@ -44,9 +44,8 @@ export default function cheats(){
         0% { transform: scale(1); }
         50% { transform: scale(1.1); }
         100% { transform: scale(1); }  
-      }
-
-
+      }      
+      
             
       .iconosFlex{
         display: flex;
